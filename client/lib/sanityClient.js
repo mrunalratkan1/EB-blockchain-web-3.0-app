2ca669de91327081cce1e886e7d3278965aa8b4e
Nikhil <nikhilrana884@gmail.com>
2022-05-29 16:57:55 +0530

    sanityClient added

--- client/lib/sanityClient.js
+++ client/lib/sanityClient.js
@@ -1,10 +1,10 @@
 import sanityClient from '@sanity/client'
 
 export const client = sanityClient({
-  projectId: 'nw03is8d',
+  projectId: 'h5bbhelo',
   dataset: 'production',
   apiVersion: 'v1',
   token:
-    'skwR1AJMMGYeNnKR0EcBvjEas7tGeslX9fEFwj1ZAwC4JCZeFXGdJAl6AFUMXeSmUkxTBJ7LPgvWjbrEzCzpx5ZBWcWZrsBM1y6L6e5xyX1Fr7I5UK6i7lmFAtx4vow1SU3xsjtSySAWlS4uIapo77YZ76MtLy7Gf5W6SnMzfnA3EEkUPFee',
+    'skarcdfoN1nvQUYKQfoU8mroaZGcBYHfqxWGjsaVjGJt7494tSjXSL5TWdE9ghpQRmhTJY6cLhSvXUegWfHurGy2lF5zZfbfXnqJTr0kg5bYhTlG5DUbQmLb92TIOBn86aZwlopnmXfB3Afcf38DRsvMTQRMN2RmlKIrXKOl8UMKzYpi0VWE',
   useCdn: false,
 })
